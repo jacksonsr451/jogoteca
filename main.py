163760234d654a68fe18df904e009a0c70ace777
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-04-13 18:20:49 -0300

    Add lista dinâmica para o template

--- main.py
+++ main.py
@@ -3,9 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 @app.route('/inicio')
 def inicio():
-    return render_template('lista.html')
+    lista = [
+        Jogo('Super Mario', 'Plataforma', 'SNES'),
+        Jogo('Pokemon Gold', 'RPG', 'GameBoy'),
+        Jogo('Mortal Kombat', 'Luta', 'SNES'),
+    ]
+
+    return render_template('lista.html', titulo="Jogos", header="Jogoteca", jogos=lista)
 
 
 app.run(host='0.0.0.0', port=8080)
